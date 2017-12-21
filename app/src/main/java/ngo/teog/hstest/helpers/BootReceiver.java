132509a9350a92d2e61154938dd448773524ec68
nitelow <julian.deyerler@teog.de>
2017-12-21 17:16:17 +0100

    AlarmManager removed -> will be replaced with JobScheduler

--- app/src/main/java/ngo/teog/hstest/helpers/BootReceiver.java
+++ app/src/main/java/ngo/teog/hstest/helpers/BootReceiver.java
@@ -1,6 +1,5 @@
 package ngo.teog.hstest.helpers;
 
-import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -14,27 +13,18 @@ import ngo.teog.hstest.R;
 
 /**
  * Globaler Receiver, der getriggert wird, wenn das Android-Gerät gebootet hat.
- * Der Receiver erstellt einen AlarmManager, der alle paar Minuten prüft, ob eine Wartung/Reparatur
- * ansteht und dementsprechend Push-Benachrichtigungen erstellt.
+ * Der Receiver erstmal nur eine Testbenachrichtigung. Da die wiederkehrende Prüfung auf abgelaufene
+ * Deadlines mithilfe der JobScheduler-API laufen wird, bin ich mir noch nocht sicher, ob dieser
+ * Receiver bestehen bleibt.
  *
- * Es muss auch beim Start der App immer geprüft wird, ob dieser AlarmManager läuft, weil
- * ein Gerät unter Umständen lange Zeit nicht neu gestartet wird. So lange wird auch dieser
- * Receiver nicht getriggert.
  * Created by Julian on 12.12.2017.
  */
 
 public class BootReceiver extends BroadcastReceiver {
-    private AlarmManager alarmManager;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            if(alarmManager == null) {
-                alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-
-                //TODO Geräte prüfen und Benachrichtigungen erstellen.
-            }
-
             //Test Benachrichtigung
             int mNotificationId = 0;
             String CHANNEL_ID = "dummy_channel";
