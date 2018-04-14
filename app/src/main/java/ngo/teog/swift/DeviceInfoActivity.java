ed0c1c2e58e076f4a0b53cd3bde932b29ece2f25
nitelow <julian.deyerler@teog.de>
2018-04-14 17:54:06 +0200

    added share button to device activity

--- app/src/main/java/ngo/teog/swift/DeviceInfoActivity.java
+++ app/src/main/java/ngo/teog/swift/DeviceInfoActivity.java
@@ -9,6 +9,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
@@ -74,6 +76,13 @@ public class DeviceInfoActivity extends AppCompatActivity {
         serialNumberView.setText(device.getSerialNumber());
 
         new DownloadImageTask(imageView, progressBar).execute("https://teog.virlep.de/device_graphics/" + device.getID() + ".jpg", null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_device_info, menu);
+        return true;
     }
 
     public void startReportActivity(View view) {
