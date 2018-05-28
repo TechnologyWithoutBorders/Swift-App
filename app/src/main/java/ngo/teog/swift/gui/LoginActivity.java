9e74abf39c282b56a530a91747b451838304466c
nitelow <julian.deyerler@teog.de>
2018-05-28 15:38:37 +0200

    added information button to login activity

--- app/src/main/java/ngo/teog/swift/gui/LoginActivity.java
+++ app/src/main/java/ngo/teog/swift/gui/LoginActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,6 +51,13 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             //TODO
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_login, menu);
+        return true;
     }
 
     public void login(View view) {
