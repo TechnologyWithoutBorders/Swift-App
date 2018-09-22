aad0859abe5885059c877a79ae75d3ce2224bb1e
nitelow <julian.deyerler@teog.de>
2018-09-22 12:36:49 +0200

    files directory is now cleared at logout

--- app/src/main/java/ngo/teog/swift/gui/main/MainActivity.java
+++ app/src/main/java/ngo/teog/swift/gui/main/MainActivity.java
@@ -1,23 +1,22 @@
 package ngo.teog.swift.gui.main;
 
-import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -28,18 +27,19 @@ import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 
+import java.io.File;
+
 import ngo.teog.swift.communication.RequestFactory;
 import ngo.teog.swift.communication.VolleyManager;
 import ngo.teog.swift.gui.AboutActivity;
 import ngo.teog.swift.gui.DeviceInfoActivity;
 import ngo.teog.swift.gui.HospitalActivity;
+import ngo.teog.swift.gui.ImageActivity;
 import ngo.teog.swift.gui.LoginActivity;
 import ngo.teog.swift.gui.NewDeviceActivity;
 import ngo.teog.swift.R;
 import ngo.teog.swift.gui.UserProfileActivity;
-import ngo.teog.swift.helpers.AlarmReceiver;
 import ngo.teog.swift.helpers.Defaults;
-import ngo.teog.swift.helpers.HospitalDevice;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -211,6 +211,10 @@ public class MainActivity extends AppCompatActivity {
         editor.remove(Defaults.COUNTRY_PREFERENCE);
         editor.remove(Defaults.NOTIFICATION_COUNTER);
         editor.apply();
+
+        for(File file : getFilesDir().listFiles()) {
+            file.delete();
+        }
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
