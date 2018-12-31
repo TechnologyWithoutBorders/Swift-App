20df9e2582e97d46609595117ac2edfef15b38af
nitelow <julian.deyerler@teog.de>
2018-12-31 22:23:48 +0100

    rearranged layout

--- app/src/main/java/ngo/teog/swift/gui/main/SearchFragment.java
+++ app/src/main/java/ngo/teog/swift/gui/main/SearchFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
