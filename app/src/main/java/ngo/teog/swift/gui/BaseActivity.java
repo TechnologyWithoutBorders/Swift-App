0e76cd63b9835d2b5bbd19c02af2c499bf461410
nitelow <julian.deyerler@teog.de>
2019-01-03 19:49:59 +0100

    smaller corrections

--- app/src/main/java/ngo/teog/swift/gui/BaseActivity.java
+++ app/src/main/java/ngo/teog/swift/gui/BaseActivity.java
@@ -1,6 +1,7 @@
 package ngo.teog.swift.gui;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
@@ -14,7 +15,12 @@ public abstract class BaseActivity extends AppCompatActivity {
 
         builder.setMessage(getString(stringID))
                 .setTitle("Information")
-                .setIcon(R.drawable.ic_info_outline_black_24dp);
+                .setIcon(R.drawable.ic_info_outline_black_24dp)
+                .setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //ignore
+            }
+        });
 
         AlertDialog dialog = builder.create();
         dialog.show();
@@ -23,10 +29,14 @@ public abstract class BaseActivity extends AppCompatActivity {
     public boolean checkForInternetConnection() {
         ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if(cm != null) {
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
-        if(activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
-            return true;
+            if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
