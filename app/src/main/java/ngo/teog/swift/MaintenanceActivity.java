228943013213a66fef214f1e489fd4829f52c433
nitelow <julian.deyerler@teog.de>
2019-04-27 20:35:34 +0200

    added maintenance Activity and stats WebView

---
+++ app/src/main/java/ngo/teog/swift/MaintenanceActivity.java
@@ -0,0 +1,13 @@
+package ngo.teog.swift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MaintenanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintenance);
+    }
+}
