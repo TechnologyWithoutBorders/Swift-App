ecceee1651db8b18fb7461da5454e132c2797e3c
nitelow <julian.deyerler@teog.de>
2019-06-01 21:46:22 +0200

    added date converter for database

--- app/src/main/java/ngo/teog/swift/helpers/data/HospitalDatabase.java
+++ app/src/main/java/ngo/teog/swift/helpers/data/HospitalDatabase.java
@@ -2,8 +2,10 @@ package ngo.teog.swift.helpers.data;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 
 @Database(entities = {User.class, Hospital.class, HospitalDevice.class, Report.class}, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
 public abstract class HospitalDatabase extends RoomDatabase {
     public abstract HospitalDao getHospitalDao();
 }
