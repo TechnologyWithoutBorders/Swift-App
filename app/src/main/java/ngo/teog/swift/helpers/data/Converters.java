e3cef51ff440c8d03e786e3b514aee3ad619a864
nitelow <julian.deyerler@teog.de>
2019-10-30 23:34:42 +0100

    added documentation

--- app/src/main/java/ngo/teog/swift/helpers/data/Converters.java
+++ app/src/main/java/ngo/teog/swift/helpers/data/Converters.java
@@ -4,12 +4,28 @@ import androidx.room.TypeConverter;
 
 import java.util.Date;
 
+/**
+ * This class is used for converting complex data structures into simple ones
+ * in order to store them in the database.
+ * @author nitelow
+ */
 public class Converters {
+
+    /**
+     * Converts a given timestamp into a Date object.
+     * @param value Timestamp
+     * @return Date
+     */
     @TypeConverter
     public static Date fromTimestamp(Long value) {
         return value == null ? null : new Date(value);
     }
 
+    /**
+     * Converts a given Date object into a timestamp.
+     * @param date Date
+     * @return Timestamp
+     */
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
