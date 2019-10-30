eef317884fb53dc0f878d0508a8a27a9682af488
nitelow <julian.deyerler@teog.de>
2019-10-30 22:39:23 +0100

    added documentation

--- app/src/main/java/ngo/teog/swift/helpers/ResourceKeys.java
+++ app/src/main/java/ngo/teog/swift/helpers/ResourceKeys.java
@@ -2,22 +2,51 @@ package ngo.teog.swift.helpers;
 
 /**
  * This class contains all standardized resource keys used within the application and for server communication.
- * @author Julian Deyerler
+ * @author nitelow
  */
 public final class ResourceKeys {
+    /** Identifier for device IDs */
     public static final String DEVICE_ID = "device_id";
+
+    /** Identifier for device objects */
     public static final String DEVICE = "device";
+
+    /** Identifier for device object lists */
     public static final String DEVICES = "devices";
+
+    /** Identifier for user IDs */
     public static final String USER_ID = "user_id";
+
+    /** Identifier for user objects */
     public static final String USER = "user";
+
+    /** Identifier for user object lists */
     public static final String USERS = "users";
+
+    /** Identifier for report IDs */
     public static final String REPORT_ID = "report_id";
+
+    /** Identifier for report objects */
     public static final String REPORT = "report";
+
+    /** Identifier for report object lists */
     public static final String REPORTS = "reports";
+
+    /** Identifier for all kinds of images */
     public static final String IMAGE = "image";
+
+    /** Identifier for all kinds of paths */
     public static final String PATH = "path";
+
+    /** Identifier for hospital objects */
     public static final String HOSPITAL = "hospital";
+
+    /** Identifier for previous states used in reports */
     public static final String REPORT_OLD_STATE = "old_state";
+
+    /** Identifier for current states used in reports */
     public static final String REPORT_NEW_STATE = "new_state";
+
+    /** Identifier for all kinds of data */
     public static final String DATA = "data";
 }
