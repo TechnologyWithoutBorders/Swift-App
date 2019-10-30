3c12cae2574d7591396b3218c11064ff93133d80
nitelow <julian.deyerler@teog.de>
2019-10-30 23:13:43 +0100

    added documentation

--- app/src/main/java/ngo/teog/swift/helpers/HospitalInfo.java
+++ app/src/main/java/ngo/teog/swift/helpers/HospitalInfo.java
@@ -6,6 +6,12 @@ import java.util.List;
 import ngo.teog.swift.helpers.data.DeviceInfo;
 import ngo.teog.swift.helpers.data.User;
 
+/**
+ * Contains all necessary information about a hospital.
+ * This class does not implement any database attributes and is therefore not used
+ * for database communication.
+ * @author nitelow
+ */
 public class HospitalInfo {
     private int id;
     private String name;
@@ -27,38 +33,74 @@ public class HospitalInfo {
         this.devices = devices;
     }
 
+    /**
+     * Returns ID of hospital.
+     * @return ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns name of hospital.
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns location of hospital.
+     * @return location
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Returns longitude of hospital location.
+     * @return longitude of location
+     */
     public float getLongitude() {
         return longitude;
     }
 
+    /**
+     * Returns latitude of hospital location.
+     * @return latitude of location
+     */
     public float getLatitude() {
         return latitude;
     }
 
+    /**
+     * Returns date of last time the hospital has been updated.
+     * @return date of last update
+     */
     public Date getLastUpdate() {
         return lastUpdate;
     }
 
+    /**
+     * Returns list of users associated with hospital.
+     * @return list of hospital's members
+     */
     public List<User> getUsers() {
         return users;
     }
 
+    /**
+     * Returns list of devices associated with hospital.
+     * @return list of hospital's devices
+     */
     public List<DeviceInfo> getDevices() {
         return devices;
     }
 
+    /**
+     * Sets date of the last time the hospital has been updated.
+     * @param lastUpdate date of last update
+     */
     public void setLastUpdate(Date lastUpdate) {
         this.lastUpdate = lastUpdate;
     }
