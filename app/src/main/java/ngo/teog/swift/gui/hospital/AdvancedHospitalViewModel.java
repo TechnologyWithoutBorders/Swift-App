ecc4b952f1d103f48adb2ed380861c4adcdfa524
nitelow <juli.sfm@web.de>
2019-12-29 00:32:07 +0100

    removed unused code

--- app/src/main/java/ngo/teog/swift/gui/hospital/AdvancedHospitalViewModel.java
+++ app/src/main/java/ngo/teog/swift/gui/hospital/AdvancedHospitalViewModel.java
@@ -15,7 +15,6 @@ import ngo.teog.swift.helpers.data.User;
 public class AdvancedHospitalViewModel extends ViewModel {
     private LiveData<Hospital> hospital;
     private LiveData<List<User>> users;
-    private LiveData<List<DeviceInfo>> deviceInfos;
     private HospitalRepository hospitalRepo;
 
     @Inject
@@ -30,7 +29,6 @@ public class AdvancedHospitalViewModel extends ViewModel {
 
         hospital = hospitalRepo.getUserHospital(userId);
         users = hospitalRepo.getUserColleagues(userId);
-        deviceInfos = hospitalRepo.getHospitalDevices(userId);
     }
 
     public LiveData<Hospital> getHospital() {
@@ -39,9 +37,5 @@ public class AdvancedHospitalViewModel extends ViewModel {
 
     public LiveData<List<User>> getUsers() {
         return users;
-    }
-
-    public LiveData<List<DeviceInfo>> getDeviceInfos() {
-        return deviceInfos;
     }
 }
