ba9d7651b235fc022cd7ed35d2d38f425ff1f168
nitelow <juli.sfm@web.de>
2019-12-29 00:01:59 +0100

    added authentication constants

---
+++ app/src/main/java/ngo/teog/swift/helpers/AuthenticationLevel.java
@@ -0,0 +1,7 @@
+package ngo.teog.swift.helpers;
+
+public final class AuthenticationLevel {
+    public static final int DISABLED = -1;
+    public static final int DEFAULT = 0;
+    public static final int ADMIN = 1;
+}
