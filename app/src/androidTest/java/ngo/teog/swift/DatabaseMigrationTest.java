40f4a8ad5b2df61ab611cf0ef15823a7c7d7fd51
nitelow <julian.deyerler@teog.de>
2020-04-04 15:21:59 +0200

    added prototype of database migration test case

---
+++ app/src/androidTest/java/ngo/teog/swift/DatabaseMigrationTest.java
@@ -0,0 +1,51 @@
+package ngo.teog.swift;
+
+import androidx.room.testing.MigrationTestHelper;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+
+import ngo.teog.swift.helpers.data.HospitalDatabase;
+
+import static androidx.work.impl.WorkDatabaseMigrations.MIGRATION_1_2;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class DatabaseMigrationTest {
+    private static final String TEST_DB = "migration-test";
+
+    @Rule
+    public MigrationTestHelper helper;
+
+    public DatabaseMigrationTest() {
+        helper = new MigrationTestHelper(InstrumentationRegistry.getInstrumentation(), HospitalDatabase.class.getCanonicalName(), new FrameworkSQLiteOpenHelperFactory());
+    }
+
+    @Test
+    public void migrate1To2() throws IOException {
+        SupportSQLiteDatabase db = helper.createDatabase(TEST_DB, 1);
+
+        // db has schema version 1. insert some data using SQL queries.
+        // You cannot use DAO classes because they expect the latest schema.
+        //db.execSQL(...);
+
+        // Prepare for the next version.
+        db.close();
+
+        // Re-open the database with version 2 and provide
+        // MIGRATION_1_2 as the migration process.
+        db = helper.runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_1_2);
+
+        // MigrationTestHelper automatically verifies the schema changes,
+        // but you need to validate that the data was migrated properly.
+    }
+}
