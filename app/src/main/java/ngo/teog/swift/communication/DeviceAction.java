d5daf374ddc37c4cbe0afb05fdce3e06c7389927
nitelow <julian.deyerler@teog.de>
2020-09-27 20:48:44 +0200

    removed unused code

--- app/src/main/java/ngo/teog/swift/communication/DeviceAction.java
+++ app/src/main/java/ngo/teog/swift/communication/DeviceAction.java
@@ -1,7 +1,6 @@
 package ngo.teog.swift.communication;
 
 public final class DeviceAction {
-    public static final String SEARCH_DEVICE = "search_device";
     public static final String FETCH_DEVICE_IMAGE = "fetch_device_image";
     public static final String FETCH_DEVICE_IMAGE_HASH = "fetch_device_image_hash";
 }
