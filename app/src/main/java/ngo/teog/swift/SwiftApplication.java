8cc465ceb84e0e8385cbf61153621576a8904c4e
nitelow <julian.deyerler@teog.de>
2021-01-24 01:40:09 +0100

    add application base class

---
+++ app/src/main/java/ngo/teog/swift/SwiftApplication.java
@@ -0,0 +1,17 @@
+package ngo.teog.swift;
+
+import android.app.Application;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasAndroidInjector;
+
+public class SwiftApplication extends Application implements HasAndroidInjector {
+
+    DispatchingAndroidInjector<Object> androidInjector;
+
+    @Override
+    public AndroidInjector<Object> androidInjector() {
+        return androidInjector;
+    }
+}
