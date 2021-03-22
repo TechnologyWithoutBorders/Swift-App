adb6caa5aeb46862b3ec82222c3d0aa39726e8a9
nitelow <julian.deyerler@teog.de>
2021-03-22 04:22:45 +0100

    implement database update algorithm using custom observables

--- app/src/main/java/ngo/teog/swift/gui/hospital/HospitalViewModel.java
+++ app/src/main/java/ngo/teog/swift/gui/hospital/HospitalViewModel.java
@@ -11,13 +11,15 @@ import javax.inject.Inject;
 import ngo.teog.swift.helpers.data.DeviceInfo;
 import ngo.teog.swift.helpers.data.Hospital;
 import ngo.teog.swift.helpers.data.HospitalRepository;
+import ngo.teog.swift.helpers.data.Observable;
 import ngo.teog.swift.helpers.data.User;
 
 public class HospitalViewModel extends ViewModel {
     private LiveData<Hospital> hospital;
+    private LiveData<Observable> observable;
     //no mutable live data needed as the user list won't change that much
     private LiveData<List<User>> users;
-    private LiveData<List<DeviceInfo>> deviceInfos;
+    private final MutableLiveData<List<DeviceInfo>> liveDeviceInfos = new MutableLiveData<>();
     private final HospitalRepository hospitalRepo;
 
     @Inject
@@ -25,14 +27,18 @@ public class HospitalViewModel extends ViewModel {
         this.hospitalRepo = hospitalRepo;
     }
 
-    public void init(int userId) {
-        if(this.hospital != null) {
-            return;
+    public LiveData<Observable> init(int userId) {
+        if(this.observable != null) {
+            return observable;
         }
 
+        observable = hospitalRepo.loadObservable(1);
         hospital = hospitalRepo.loadUserHospital(userId, true);
         users = hospitalRepo.loadUserColleagues(userId, false);
-        deviceInfos = hospitalRepo.loadHospitalDevices(userId, false);
+
+        new Thread(new DeviceInfoLoadRunner(userId)).start();
+
+        return observable;
     }
 
     public LiveData<Hospital> getHospital() {
@@ -44,6 +50,24 @@ public class HospitalViewModel extends ViewModel {
     }
 
     public LiveData<List<DeviceInfo>> getDeviceInfos() {
-        return deviceInfos;
+        return liveDeviceInfos;
+    }
+
+    public void refreshDeviceInfos(int userId) {
+        new Thread(new DeviceInfoLoadRunner(userId)).start();
+    }
+
+    private class DeviceInfoLoadRunner implements Runnable {
+        private final int userId;
+
+        public DeviceInfoLoadRunner(int userId) {
+            this.userId = userId;
+        }
+
+        @Override
+        public void run() {
+            List<DeviceInfo> deviceInfos = hospitalRepo.getHospitalDevices(userId);
+            liveDeviceInfos.postValue(deviceInfos);
+        }
     }
 }
