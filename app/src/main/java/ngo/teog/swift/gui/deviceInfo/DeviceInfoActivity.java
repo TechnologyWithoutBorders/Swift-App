949d4238109953f74fc81a9ce09091bf739869db
nitelow <julian.deyerler@teog.de>
2022-10-22 22:46:32 +0200

    remove unused import

--- app/src/main/java/ngo/teog/swift/gui/deviceInfo/DeviceInfoActivity.java
+++ app/src/main/java/ngo/teog/swift/gui/deviceInfo/DeviceInfoActivity.java
@@ -42,7 +42,6 @@ import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
